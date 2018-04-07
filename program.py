cdd26646e1d0c44be5fcf1bd8865c7a49c678bf4
Unknown <emmanuel.mars@epsi.fr>
2018-04-07 08:27:15 +0200

    2TU: les deux qui marchent pour wake travis up

--- program.py
+++ program.py
@@ -51,7 +51,7 @@ class TestFonction(unittest.TestCase):
   cir =conn.cursor() 
   cir.execute("Select age from membres where nom='Blumar'")    
   first=cir.fetchone()[0]
-  self.assertEqual(first,18)
+  self.assertEqual(first,15)
   
  
   
