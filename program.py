b5365966e29777bab1782acd393f6f9541c1ff01
Unknown <emmanuel.mars@epsi.fr>
2018-04-24 15:44:41 +0200

    Test de Travis

--- program.py
+++ program.py
@@ -1,15 +1,15 @@
 # -*-coding:Latin-1 -*
-import os 
+import os
 import sqlite3
 import unittest
 import logging
 
-fichierDonnees ="bd_test.sq3" 
+fichierDonnees ="bd_test.sq3"
 conn =sqlite3.connect(fichierDonnees)
 conn.text_factory = str
 cur =conn.cursor()
 car =conn.cursor()
-cir =conn.cursor() 
+cir =conn.cursor()
 
 # cur.execute("CREATE TABLE membres (age INTEGER, nom TEXT, taille REAL)")
 # cur.execute("INSERT INTO membres(age,nom,taille) VALUES(21,'Dupont',1.83)")
@@ -18,18 +18,18 @@ cir =conn.cursor()
 
 # cur.execute("DELETE FROM membres where age >1")
 
-class TestFonction(unittest.TestCase): 
+class TestFonction(unittest.TestCase):
     # Chaque méthode dont le nom commence par 'test_'
-    # est un test.
-	
- def setUp(self): 
+    # est un test1.
+
+ def setUp(self):
   conn =sqlite3.connect(fichierDonnees)
   cur =conn.cursor()
   car =conn.cursor()
   cir =conn.cursor()
-  print("SetUp") 
-   
- 
+  print("SetUp")
+
+
  def tearDown(self):
   #conn.commit()
   #cur.close()
@@ -39,22 +39,22 @@ class TestFonction(unittest.TestCase):
   print(" ")
   print("TearDown")
   os.system("pause")
-  
+
  def test_get_element(self):
-  
-  cir.execute("Select age from membres where nom='Dupont'")    
+
+  cir.execute("Select age from membres where nom='Dupont'")
   first=cir.fetchone()[0]
   self.assertEqual(first,21)
-  
- def test_get_element2(self):  
+
+ def test_get_element2(self):
   conn =sqlite3.connect(fichierDonnees)
-  cir =conn.cursor() 
-  cir.execute("Select age from membres where nom='Blumar'")    
+  cir =conn.cursor()
+  cir.execute("Select age from membres where nom='Blumar'")
   first=cir.fetchone()[0]
   self.assertEqual(first,15)
-  
- 
-  
+
+
+
 cur.execute("select name from sqlite_master where type = 'table'")
 print("----Liste des tables de la BDD SQLite3----")
 print(list(cur))
@@ -64,5 +64,5 @@ print(list(car))
 
 if __name__ == '__main__':
     unittest.main()
-    print("THE END")   
+    print("THE END")
     os.system("pause")
